e1576af547d023a39bff606b712054edfbf82408
Joel Schumacher <joelschum@gmail.com>
2018-06-26 12:38:36 +0000

    Improve quote message formatting and add support for discord message links

--- commands/quote.lua
+++ commands/quote.lua
@@ -2,11 +2,14 @@ local registry = require("./registry")
 local util = require("./util")
 local client = require("../client")
 
-registry.add(util.oneArgPatterns("quote", true), function(message, arg)
-    local quoteChannelId = message.channel.id
-    local quoteMsgId, message = arg:match("^(%d+)%s+(.*)$")
-    if not quoteMsgId then
-        quoteChannelId, quoteMsgId, message = arg:match("^(%d+):(%d+)%s+(.*)$")
+registry.add({"^!quote%s+(%S+)$", "^!quote%s+(%S+)%s+.*$"}, function(message, arg)
+    local quoteChannelId, quoteMsgId = arg:match("^<?https://discordapp.com/channels/%d+/(%d+)/(%d+)>?$")
+    if not quoteChannelId or not quoteMsgId then
+        quoteChannelId, quoteMsgId = arg:match("^(%d+):(%d+)$")
+        if not quoteChannelId or not quoteMsgId then
+            quoteChannelId = message.channel.id
+            quoteMsgId = arg:match("^(%d+)$")
+        end
     end
 
     if not quoteChannelId or not quoteMsgId then
@@ -19,10 +22,11 @@ registry.add(util.oneArgPatterns("quote", true), function(message, arg)
     end
 
     local quoteMsg = quoteChannel:getMessage(quoteMsgId)
-    if not quoteMsgId then
-        return "Channel does not exist."
+    if not quoteMsg then
+        return "Message does not exist in this channel."
     end
 
-    return ("<@%d> said in %s at %s: \"\" - %s"):format(quoteMsg.author.id,
-        tostring(quoteChannel.name), quoteMsg.timestamp, quoteMsg.content, message)
-end, "!quote <messageid> text, !quote <channelid>:<messageid> text", "Quote a message")
+    return ("<@%d> said in %s at %s:\n*%s*"):format(quoteMsg.author.id,
+        quoteChannel.mentionString, os.date("!%Y.%m.%d %H:%M:%S UTC", quoteMsg.createdAt),
+        quoteMsg.content:gsub("%*", "\\*"))
+end, "!quote <messageid>, !quote <channelid>:<messageid>", "Quote a message")
